4e15e9156d282af652cf0bc49029d83b2cec69e1
Igor C. Batista <igor.batista@gmail.com>
2026-05-05 09:03:03 -0300

    fix: Tasks Hoje e Tasks Mês contam tarefas criadas no período, não por lançamentos
    daily_task_count e monthly_task_count usavam work_date dos task_items,
    contando tarefas que apenas receberam lançamentos no dia. A semântica correta
    é contar tarefas criadas (start_date) no período.

--- app/controllers/concerns/dashboard_calculations.rb
+++ app/controllers/concerns/dashboard_calculations.rb
@@ -23,11 +23,11 @@ module DashboardCalculations
   end
 
   def calculate_daily_task_count
-    Task.joins(:task_items).where(task_items: { work_date: Date.current }).distinct.count
+    Task.where(start_date: Date.current).count
   end
 
   def calculate_monthly_task_count
-    Task.joins(:task_items).where(task_items: { work_date: Date.current.all_month }).distinct.count
+    Task.where(start_date: Date.current.all_month).count
   end
 
   def calculate_daily_value
